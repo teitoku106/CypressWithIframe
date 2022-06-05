d29b3d4fa45f175b653e8fd1968652a110641fdb
TamPS (Kyle) <73167411+teitoku106@users.noreply.github.com>
2022-06-05 14:24:27 +0700

    Add files via upload

---
+++ cypress/integration/regressionTest/demo.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+import * as commonFunction from "../../functions/client/common-functions";
+
+describe("example with Cypress", () => {
+  beforeEach(async () => {
+    await commonFunction.navigateTo(Cypress.env("HOME_URL"));
+  });
+
+  it("createNewUser", async () => {
+    //input [Your name] - [Phone Number] - [Email address] - [Company name]
+    await commonFunction.createNewClient();
+    //input [Select type of Industry]
+    await commonFunction.selectIndustry("Health");
+    //Click checkbox for Terms And Conditions
+    await commonFunction.termsAndConditionsCheckBox();
+  });
+});
