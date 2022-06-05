4ff662fc108c2d401ad0e604472afee5f5231c31
TamPS (Kyle) <73167411+teitoku106@users.noreply.github.com>
2022-06-05 21:52:59 +0700

    Add files via upload

--- cypress/functions/web/common-functions.js
+++ cypress/functions/web/common-functions.js
@@ -9,48 +9,35 @@ export async function navigateTo(urlInput) {
 }
 
 export async function createNewClient() {
-  await cy.enter("#prospaFormFrame").then((getBody) => {
+  await cy.enter(homePage.iframe).then((getBody) => {
     getBody()
-      .find(homePage.yourName)
-      .should("be.visible")
-      .click({ timeout: 10000 });
+      .find(homePage.yourName).should("be.visible").click({ timeout: 10000 });
     getBody()
-      .find(homePage.yourName)
-      .type(userData["new_account"][0]["yourName"]);
+      .find(homePage.yourName).type(userData["new_account"][0]["yourName"]);
     getBody()
-      .find(homePage.phoneNumber)
-      .should("be.visible")
-      .click({ timeout: 10000 });
+      .find(homePage.phoneNumber).should("be.visible").click({ timeout: 10000 });
     getBody()
-      .find(homePage.phoneNumber)
-      .type(userData["new_account"][0]["phoneNumber"]);
+      .find(homePage.phoneNumber).type(userData["new_account"][0]["phoneNumber"]);
     getBody()
-      .find(homePage.emailAddress)
-      .should("be.visible")
-      .click({ timeout: 10000 });
+      .find(homePage.emailAddress).should("be.visible").click({ timeout: 10000 });
     getBody()
-      .find(homePage.emailAddress)
-      .type(userData["new_account"][0]["emailAddress"]);
+      .find(homePage.emailAddress).type(userData["new_account"][0]["emailAddress"]);
     getBody()
-      .find(homePage.companyName)
-      .should("be.visible")
-      .click({ timeout: 10000 });
+      .find(homePage.companyName).should("be.visible").click({ timeout: 10000 });
     getBody()
-      .find(homePage.companyName)
-      .type(userData["new_account"][0]["companyName"]);
+      .find(homePage.companyName).type(userData["new_account"][0]["companyName"]);
   });
 }
 
 export async function selectIndustry(type) {
-  await cy.enter("#prospaFormFrame").then((getBody) => {
+  await cy.enter(homePage.iframe).then((getBody) => {
     getBody().find(homePage.industry).should("be.visible").select(type);
   });
 }
 
 export async function termsAndConditionsCheckBox() {
-  await cy
-    .iframe("#prospaFormFrame")
-    .find(homePage.termAndConditionsCheckBox)
-    .should("be.visible")
-    .click({ force: true });
+  await cy.enter(homePage.iframe).then((getBody) => {
+    getBody()
+      .find(homePage.termAndConditionsCheckBox).should("be.visible").click({ force: true });
+  });
 }
